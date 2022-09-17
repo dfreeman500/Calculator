0c03963d049139145540d771304a8c7b26135a4f
Dan <danielfr500@gmail.com>
2022-09-17 11:32:43 -0400

    Add project files.

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calculator
+    {
+
+        public static void Addition()
+        {
+            Console.WriteLine("5+5");
+        }
+        public static void Subtraction()
+        {
+            Console.WriteLine("8-2");
+        }
+        public static void Multiplication()
+        {
+            Console.WriteLine("9*9");
+        }
+        public static void Division()
+        {
+            Console.WriteLine("100/2");
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using Calculator;
+
+while(true){
+
+    string userInput = Display();
+
+    try
+    {
+        int convertedUserInput = Int32.Parse(userInput);
+        if (convertedUserInput == 6)
+        {
+            break;
+        }
+        Decision(convertedUserInput);
+
+
+    }
+    catch
+    {
+        //TODO break down errors from specific to general
+        Console.WriteLine("Please enter just the number that corresponds to your choice.");
+    }
+
+  
+
+
+
+}
+
+static string Display()
+{
+    Console.WriteLine("\n1. Addition");
+    Console.WriteLine("2. Subtraction");
+    Console.WriteLine("3. Multiplication");
+    Console.WriteLine("4. Division");
+    Console.WriteLine("5. Access Calculator History");
+    Console.WriteLine("6. Exit");
+
+    Console.WriteLine("Enter the number that corresponds to what you would like to do: (eg: 3");
+    string userInput = Console.ReadLine();
+    return userInput;
+}
+
+
+static void Decision(int convertedUserInput)
+{
+    switch (convertedUserInput)
+    {
+        case 1:
+            Calculator.Calculator.Addition();
+            break;
+        case 2:
+            Calculator.Calculator.Subtraction();
+            break;
+        case 3:
+            Calculator.Calculator.Multiplication();
+            break;
+        case 4:
+            Calculator.Calculator.Division();
+            break;
+        case 5:
+            break;
+        default:
+            Console.WriteLine("Please enter a number corresponding to the choices.");
+            break;
+     }
+}
