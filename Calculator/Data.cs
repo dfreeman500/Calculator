8a8d5f2f4a5ed1a96f0a9069095b2576306f29c8
Dan <danielfr500@gmail.com>
2022-09-19 13:44:14 -0400

    Save each calculation to SQL

--- Calculator/Data.cs
+++ Calculator/Data.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Data.SqlClient;
 
-public class Data
-{
-	public static void Save()
+namespace Data {
+	public class Data
 	{
+		public static void Save((decimal, decimal, string, decimal) returnedValues)
+		{
+
+			SqlConnection sqlConnection;  //ctrl . - install package "System.DataSqlClient"
+
+			//can get connection string by going to SQL Server Object Explorer, right click DB, then click properties
+			string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=CalculatorDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+			try
+			{
+				sqlConnection = new SqlConnection(connectionString);
+				sqlConnection.Open();
+				Console.WriteLine("Connection successful");
+
+				 string insertQuery = "INSERT INTO CALCULATIONS (num_one, operator, num_two, equals, result, datetime) VALUES('" + returnedValues.Item1 + "', '" + returnedValues.Item3 + "' ,'" + returnedValues.Item2 + "', '=', '" + returnedValues.Item4 + "', '" + DateTime.Now + "')";
+				
+				SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+				insertCommand.ExecuteNonQuery();
+				Console.WriteLine("data committed");
+
+
+		}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+		}
 	}
+
 }
